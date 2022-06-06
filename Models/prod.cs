3d3333595d515c927a47d513b6baee1642a33209
olobakehinde <olobakehinde@gmail.com>
2022-06-06 15:05:25 +0100

    models updated

--- Models/prod.cs
+++ Models/prod.cs
@@ -1,6 +1,16 @@
-﻿namespace contoso.Models
+﻿using System.Text.Json;
+
+namespace contoso.Models
 {
     public class prod
     {
+        public string id { get; set;}
+        public string image{ get; set;}
+        public string maker { get; set; }
+        public string tittle{ get; set; }
+        public string description  { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<prod>(this);
     }
+
 }
+
